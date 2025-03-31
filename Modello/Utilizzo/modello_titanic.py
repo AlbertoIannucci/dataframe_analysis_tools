a4a96d0de7756015a06e41813818ce795116b513
Alberto Iannucci <iannuccialberto@virgilio.it>
2025-03-31 12:03:04 +0200

    Update modello_titanic.py

--- Modello/Utilizzo/modello_titanic.py
+++ Modello/Utilizzo/modello_titanic.py
@@ -1,17 +1,5 @@
 from modello_base import ModelloBase
 import pandas as pd
-# Passo 1. Analisi generale del dataframe - Valori univoci
-# Passo 2. Ripulire il dataframe dalle variabili che non servono per l'analisi
-# Passo 3. Ripulire il dataframe dalle variabili che contengono più valori nulli
-# Passo 4. Per le variabili che contengono pochi valori nan,
-# si effettuano delle sostituzioni:
-# se i valori sono numerici si sceglie tra la media (con pochi valori outleirs)
-# oppure la mediana. (Per pochi valori outliers s'intende minore del 15%/10%)
-# se i valori degli outliers cresce notevolmente con la sostituzione:
-# si può eseguire la mediana su gruppi (significativi) per migliorare la situazione
-# Passo 5. È consigliato sostituire i valori stringhe con valori numerici (rimappatura
-# Passo 6.	Possiamo modificare i nomi delle colonne (Facoltativa)
-
 
 class ModelloTitanic(ModelloBase):
 
@@ -27,7 +15,6 @@ class ModelloTitanic(ModelloBase):
         # Drop dell'osservazione con tutti i valori nulli nan
         df_sistemato = df_sistemato.drop(index=1309, axis=0).copy()
         # Sostituzione valori nan di age con mediana
-        #df_sistemato["age"] = df_sistemato["age"].fillna(df_sistemato["age"].median())
         df_sistemato["age"] = (df_sistemato.groupby(["pclass", "sex"])["age"]
                                .apply(lambda x: x.fillna(x.median()))).reset_index(level=[0,1], drop=True)
         # Rimapapatura colonna sex (0:female, 1:male)
@@ -53,8 +40,7 @@ class ModelloTitanic(ModelloBase):
 
 # Utilizzo modello
 modello = ModelloTitanic("../dataset/data_04.csv")
-#modello.analisi_generali(modello.dataframe_sistemato)
-#modello.analisi_valori_univoci(modello.dataframe_sistemato, ["Età", "Fratelli/Coniugi", "Genitori/Figli"])
-#modello.individuazione_outliers(modello.dataframe_sistemato, ["Genere"])
-#modello.dataframe_sistemato.to_csv("../dataset_sistemati/data_04.csv", index=False)
+modello.analisi_generali(modello.dataframe_sistemato)
+modello.analisi_valori_univoci(modello.dataframe_sistemato, ["Età", "Fratelli/Coniugi", "Genitori/Figli"])
+modello.individuazione_outliers(modello.dataframe_sistemato, ["Genere"])
 modello.analisi_indici_statistici(modello.dataframe)
